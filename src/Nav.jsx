9a2d508c1eca0b48bf7292feb3387ad06f67194b
Minh Vy Ha <88866539+minhvy828@users.noreply.github.com>
2022-07-31 13:21:08 +1000

    Add files via upload

---
+++ src/Nav.jsx
@@ -0,0 +1,12 @@
+import './Nav.css'
+
+export default function Nav(){
+    return (
+        <>
+            <nav>
+                <img src='src/assets/web.svg' alt="" />
+                <h6>my travel journal</h6>
+            </nav>
+        </>
+    )
+}
